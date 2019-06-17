65cb51134dbd1629358fe3f3d95b9ed7e6362b2b
Roman <34662053+ParshutinRoman@users.noreply.github.com>
2019-06-18 00:13:03 +0300

    Add files via upload

--- Class.py
+++ Class.py
@@ -16,16 +16,8 @@ class Animals():
                 " of forage")
         print(info)
 
-    def feed_animal(self, forage=1000):
-        """Spend some forage"""
-        forage -= self.ration
-        self.weight += self.ration / 20
-
     def says (self):
         print(self.name + "says" + self.voice + "twice")
-
-    def kill_animal(self, meat):
-        meat += self.weight * 0.45
 
 
 class Birds(Animals):
@@ -65,6 +57,11 @@ chicken2 = Birds("Кукареку", 2.5, 0.5, "КоКО", 4)
 duck = Birds("Кряква", 3.4, 0.5, "КряКря", 1)
 
 animals = (sheep1, sheep2, goat2, goat1, goose1, goose2, cow, chicken1, chicken2, duck)
+forage = 1000
+meat = 0
+milk = 0
+eggs = 0
+wool = 0
 
 def heaviest_animal():
     max_weight = 0
@@ -81,41 +78,28 @@ def total_weight():
         total_weight += animal.weight
     print("Общий вес обитателей фермы - " + str(total_weight))
 
+def feed_animal():
+    """Spend some forage"""
+    forage = 1000
+    for animal in animals:
+        forage -= animal.ration
+        animal.weight += animal.ration / 20
+    print(forage)
+
+
+
 goat2.show_animal()
-goat2.feed_animal()
 goat2.show_animal()
 print(sheep1.weight)
 print(animals)
 heaviest_animal()
+total_weight()
+feed_animal()
 total_weight()
 
 
 
 
 
-# Вы приехали помогать на ферму Дядюшки Джо и видите вокруг себя множество разных животных:
-#
-# гусей "Серый" и "Белый"
-# корову "Маньку"
-# овец "Барашек" и "Кудрявый"
-# кур "Ко-Ко" и "Кукареку"
-# коз "Рога" и "Копыта"
-# и утку "Кряква"
-# Со всеми животными вам необходимо как-то взаимодействовать:
-#
-# кормить
-# корову и коз доить
-# овец стричь
-# собирать яйца у кур, утки и гусей
-# различать по голосам(коровы мычат, утки крякают и т.д.)
-# Задача №1
-# Нужно реализовать классы животных, не забывая использовать наследование, определить общие методы взаимодействия с животными и дополнить их в дочерних классах, если потребуется.
-#
-# Задача №2
-# Для каждого животного из списка должен существовать экземпляр класса. Каждое животное требуется накормить и подоить/постричь/собрать яйца, если надо.
-#
-# Задача №3
-# У каждого животного должно быть определено имя(self.name) и вес(self.weight).
-#
-# Необходимо посчитать общий вес всех животных(экземпляров класса);
-# Вывести название самого тяжелого животного.
+
+
